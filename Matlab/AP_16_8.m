3829078129eb849b94b566728b2c764f23a49f35
HongCheng <1509943027@qq.com>
2017-04-14 22:43:14 +0900

    Use the Matlab to draw the raw data

---
+++ Matlab/AP_16_8.m
@@ -0,0 +1,44 @@
+x = xlsread('AP2_coordinate,rssi.xlsx','(x,y,rssi)');
+
+locationX = x(:,1);
+locationY = x(:,2);
+RSSValue = x(:,3);
+
+figure1 = figure('InvertHardcopy','off','Name','APlocation(16,8) RSSI MAP',...
+    'Color',[1 1 1]);
+
+% 创建 axes
+axes1 = axes('Parent',figure1);
+hold(axes1,'on');
+
+%% CData 标记颜色
+%
+% %矢量 - 对绘图中的每个标记使用不同的颜色。
+% 将 CData 指定为长度与 XData 相同的矢量。
+% 以线性方式将矢量中的值映射到当前颜色图中的颜色。
+
+s3 = scatter3(locationX,locationY,RSSValue,'filled','CData',RSSValue);
+title('APlocation(16,8) RSSI MAP')
+
+s3.MarkerEdgeColor = 'flat'; % 边缘颜色，跟随CData
+% s3.MarkerEdgeAlpha = 0.8; % 透明度
+colormap(jet(10));
+colorbar;
+%%
+% 创建 xlabel
+xlabel('X');
+
+% 创建 zlabel
+zlabel('RSSI value');
+
+% 创建 ylabel
+ylabel('Y');
+
+% 取消以下行的注释以保留坐标轴的 X 范围
+% xlim(axes1,[0 25]);
+% 取消以下行的注释以保留坐标轴的 Y 范围
+% ylim(axes1,[0 18]);
+% 取消以下行的注释以保留坐标轴的 Z 范围
+% zlim(axes1,[-80 -20]);
+
+grid(axes1,'on');
